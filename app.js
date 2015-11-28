e95343ef0f428fc3e1a590b79e6d8fd43c3f7d3d
mo <split@live.cn>
2015-11-28 16:59:11 +0800

    设定 配置文件 及 对应 配置的 更改

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-var express       =   require('express');       //web框架
-var session       = require('express-session');
-var dustjs        =   require('adaro');         //dustjs 渲染模块
-var path          =   require('path');          //path  解析和转码的模块
-var favicon       =   require('serve-favicon'); //favicon 显示模块
-var cookieParser  =   require('cookie-parser');
-var bodyParser    =   require('body-parser');
-var compression   =   require('compression');
+var express       =     require('express');
+var session       =     require('express-session');
+var dustjs        =     require('adaro');
+var path          =     require('path');
+var favicon       =     require('serve-favicon');
+var cookieParser  =     require('cookie-parser');
+var bodyParser    =     require('body-parser');
+var compression   =     require('compression');
+var app           =     express();
 
-var app = express();
+var cfg           =     require('./config');
 
-// 注册
 app.engine("dust",dustjs.dust({ cache: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,11 +22,11 @@ app.use(cookieParser());
 app.use(compression());
 
 app.use(session({
-    secret              :   'secret',                //关键KEY
+    secret              :   cfg.session_secret,
     resave              :   true,
     saveUninitialized   :   false,
     cookie              :   {
-        maxAge:1000*60*10           //失效时间为10分钟
+        maxAge  :   cfg.session_time
     }
 }));
 
--- config.js
+++ config.js
@@ -7,24 +7,25 @@ var path = require('path');
 var config = {
     // debug 为 true 时，用于本地调试
     debug: true,
-    site_static_host        :       '',// 静态文件存储域名
-    host                    :       'localhost',
-    mongodb                 :        'mongodb://127.0.0.1/pets_club_dev',
+    site_static_host        :           '',// 静态文件存储域名
+    host                    :           'localhost',
+    mongodb                 :           'mongodb://127.0.0.1/pets_club_dev',
 
     // redis 配置，默认是本地
-    redis_host              :         '127.0.0.1',
-    redis_port              :         6379,
-    redis_db                :         0,
+    redis_host              :           '127.0.0.1',
+    redis_port              :           6379,
+    redis_db                :           0,
 
-    session_secret          :         'pets_club_secret', // 务必修改
-    auth_cookie_name        :         'pets_club_cookie',
+    session_secret          :           'pets_club_secret', // 务必修改
+    session_time            :           1000*60*10,
+    auth_cookie_name        :           'pets_club_cookie',
 
     qn_access: {
-        accessKey           :          'YYFZ8Mv3gARmlE8-MDc-zj8Yp0p__SoQj6u_Vjuc',
-        secretKey           :          'Qpmmt-jax55bZxWVC0lLmE__QJgEAfAyGG1CUkVZ',
-        bucket              :          'baby'
+        accessKey           :           'YYFZ8Mv3gARmlE8-MDc-zj8Yp0p__SoQj6u_Vjuc',
+        secretKey           :           'Qpmmt-jax55bZxWVC0lLmE__QJgEAfAyGG1CUkVZ',
+        bucket              :           'baby'
     },
-    port                    :           80
+    port                    :           '80'//监听端口
 };
 
 if (process.env.NODE_ENV === 'test') {
