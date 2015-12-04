90afdd298e94730445e8072541076a4144ab72fc
mo <split@live.cn>
2015-12-04 22:37:23 +0800

    。。。。

---
+++ pmiddlewares/authUser.js
@@ -0,0 +1,13 @@
+/**
+ * 验证用户是否需要登录的中间件
+ * Created by WG on 2015/12/4.
+ */
+
+//用户需要验证登录
+exports.loginRequired    =   function(req, res, next){
+    if (!req.session || !req.session.user) {
+        req.session.error='请先登陆';
+        return res.redirect('/login');
+    }
+    next();
+};
