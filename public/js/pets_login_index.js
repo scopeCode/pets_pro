8712df89198a76c01864b8ae23d3cad3bbf39e26
mo <split@live.cn>
2015-12-05 18:08:47 +0800

    js

---
+++ public/js/pets_login_index.js
@@ -0,0 +1,25 @@
+/**
+ * 登录页面的JS处理
+ * Created by WG on 2015/12/5.
+ */
+
+$(document).ready(function(){
+    setTimeout(function(){
+        loginIndex.build();
+    },0);
+});
+
+var loginIndex = (function(){
+    var optIndex = {
+        build		:	function(){
+
+        },
+        bindEvent	:	function(){
+
+        },//bindEvent  end
+    };
+
+    return {
+        build		:	function(){optIndex.build();}
+    };
+})();
